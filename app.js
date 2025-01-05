63ed6a1c7478d8be2a4534e49348a03e3a4ef2eb
Santhoshkumarz <id.santhoshk@gmail.com>
2025-01-05 16:45:49 +0530

    Updated app.js for Vercel serverless deployment

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// app.js
 const express = require("express");
 const db = require("./src/config/database");
 const bodyParser = require("body-parser");
 const routes = require("./src/routes");
-const app = express();
 const dotenv = require("dotenv");
-const cors = require('cors');
-app.use(cors());
+const cors = require("cors");
+
+// Initialize environment variables
 dotenv.config();
 
-app.use(express.json());
+const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
 
+// API Routes
 app.use("/api", routes);
 
+// Root Route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.SERVERPORT || 3007;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+// Export the app for Vercel serverless deployment
 module.exports = app;
